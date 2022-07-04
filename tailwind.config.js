cc19a5d400d28e43e2e904811660c643dadf9974
chunwen <hsbsbbdn@gmail.com>
2022-07-04 23:51:19 +0800

    feat:add tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: "tw-",
+  purge: ["./src/pages/**/*.{js,ts,jsx,tsx}"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
